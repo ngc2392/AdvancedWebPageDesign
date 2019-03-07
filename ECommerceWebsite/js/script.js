6453cf87134ba22e1a53e2e226a421cf7496b942
ngc2392 <phillicon4733@gmail.com>
2019-03-06 23:57:51 -0500

    filters box, side nav

--- ECommerceWebsite/js/script.js
+++ ECommerceWebsite/js/script.js
@@ -5,7 +5,21 @@ window.onload = function() {
     
     document.getElementById("closeButton").onclick = function fun() {
         document.getElementById("sideNav").style.width = "0";
-    }
-    
+    }    
 }
 
+$(document).ready(function() {
+    
+    $('.filters-btn').click(function() {
+        
+        $('.filters-box-content').toggle('slow', function() {
+            //animation complete
+        });
+    });
+
+    $('.filters-options-box').on('click', function(e) {
+        $(this).toggleClass('filters-options-box-clicked');
+        e.preventDefault();
+    });
+    
+});
