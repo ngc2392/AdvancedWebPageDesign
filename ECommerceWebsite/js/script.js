29a35f3eeb623add659644b23b48c3907a2a1283
ngc2392 <phillicon4733@gmail.com>
2019-03-14 01:41:34 -0400

    lots of changes

--- ECommerceWebsite/js/script.js
+++ ECommerceWebsite/js/script.js
@@ -1,16 +1,36 @@
-window.onload = function() {
-    document.getElementById("menu").onclick = function fun() {
-        document.getElementById("sideNav").style.width = "250px";
-    }
-    
-    document.getElementById("closeButton").onclick = function fun() {
-        document.getElementById("sideNav").style.width = "0";
-    }    
-}
 
 $(document).ready(function() {
+
+
+    cartItems = [];
+    var itemCount = 0;
+    var priceTotal = 0.00;
+
+    document.getElementById("shopping-cart-icon").addEventListener('click', function() {
+        document.getElementById("cart-box").style.width = "250px";
+        // document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+        $("#blackOut").show();
+        $('body').addClass('overflow-hidden');
+    });
+
+
+    document.getElementById("close-btn").addEventListener('click', function() {
+        document.getElementById("cart-box").style.width = "0";
+        // document.body.style.backgroundColor = "#FFF";
+        $("#blackOut").hide();
+        $('body').removeClass('overflow-hidden');
+    });
+   
+    
+      
+      $('.add_item').click(function() {
+          
+      });
+      
+      
     
     $('.filters-btn').click(function() {
+
         
         $('.filters-box-content').toggle('slow', function() {
             //animation complete
@@ -91,19 +111,98 @@ $(document).ready(function() {
 
     
     }
-    
-});
-
-/*products.html*/
-
-$(document).ready(function() {
-    $('.add_to_cart_btn').on('click', function() {
 
     
 
-        // get an array of parents of the button
-        //we need to use .eq because [] returns a dom element
-        console.log($(this).parents().eq(2).attr("data-region"));
+    // https://stackoverflow.com/questions/6658752/click-event-doesnt-work-on-dynamically-generated-elements
+    $(".products-grid").on("click", "div.add_to_cart_btn", function() {
+        console.log("ADD TO CART CLICKED");
+
+        // https://stackoverflow.com/a/10003709/9599554
+        var idOfProduct = $(this).parents().eq(2).attr("id").match(/\d+/)[0];
+        console.log("IDOFPRODUCT", idOfProduct);
+
+        var productFromDatabase = products.find(x => x.id === parseInt(idOfProduct));
+
+        console.log(productFromDatabase);
+
+        var name = productFromDatabase.name;
+        var price = productFromDatabase.price;
+
+        itemCount++;
+        $("#itemCount").text(itemCount).css('display', 'block');
+
+        console.log($('#itemCount'));
+
+        var shoppingCartItem = document.createElement("div");
+        shoppingCartItem.innerHTML = `
+        
+        <div class="item-row">   
+                <div class="item-quantity">2x</div>
+                <div class="item-name">${name}</div>
+                <div class="item-price">${price}</div>
+                <div class="remove_item">X</div>
+        </div>
+        `;
+
+        $(this).insertAfter($('[class^="fitem-row"]').last());
+
+        var shoppingCartColumn = document.querySelector(".items-column");
+
+         shoppingCartColumn.appendChild(shoppingCartItem);
+
+         cartItems.push(shoppingCartItem);
+
+         var idOfProduct = getIdOfCard($(this).parents().eq(2));
+
+         var productFromDatabase = products.find(x => x.id === parseInt(idOfProduct));
+
+
+         var price = parseInt(productFromDatabase.price.replace("$", ""));
+         priceTotal += price;
+         $("#cart-total").text("$"+priceTotal);
+
+         
+    });
+
+
+    function getIdOfCard(card) {
+        return card.attr("id").match(/\d+/)[0];
+    }
+
+    // i think i need to use event delegation because the 
+    // items are being dynamically created.  This function only did something 
+    // when there was something in the cart hard coded in the html
+    /*
+    $('.remove_item').on('click', function() {
+        $(this).remove();
+        itemCount--;
+        $('itemCount').text(itemCount);
+    });
+    */
+
+    $("#cart-box").on('click', '.remove_item', function() {
+        
+
+        var priceOfItem = $(this).parents().eq(0).find('.item-price').text().replace("$", "");
+
+        // var priceOfItem = $(this).parents().eq(0)
+
+        // remove item row from shopping cart
+        $(this).parents().eq(0).remove();
+        
+    
+        // update cart quantity indicator
+        itemCount--;
+        $('#itemCount').text(itemCount);
+
+        // remove cost of deleted item from the total cart price 
+        priceTotal -= priceOfItem;
+        $("#cart-total").text("$" + priceTotal);
+
+        if(itemCount == 0) {
+            $('#itemCount').css('display', 'none');
+        }
     });
 
 
@@ -140,19 +239,117 @@ $(document).ready(function() {
         console.log("div[data-region*=" + filterValues + "]");
         
         jQuery.each(filterValues, function(index, item) {
-
-            $(".products-grid").find("div[data-region*=" + item + "]").each(function (i) {
-               
-               // $(this).show();
-                $(this).delay(200).slideDown("fast");
             
+            console.log( $(".products-grid").find("div[data-region*=" + item + "]"));
+            $(".products-grid").find("div[data-region*=" + item + "]").each(function (i) {
+             
+               // $(this).show();
+               $(this).delay(200).slideDown("fast");
+                
 		    });
         });
-
-        if(!filterValues) {
+// https://stackoverflow.com/a/24403771/9599554
+        if(!Array.isArray(filterValues) || !filterValues.length) {
            console.log("No filters will be applied");
+           $(".products-grid").find(".card").each(function() {
+                $(this).delay(200).slideDown("fast");
+           });
         }
     }
 
+    /*products.html*/
 
+var products = [
+    {
+        "id": 0,
+        "name":"Scandavian Blend",
+        "region": "Arabia",
+        "roast_level": "Medium",
+        "price": "$42",
+        "description": "Earthly, dark choclate, medium body. From Arabia.",
+        "weights": "12oz, 3lb, 5lb",
+        "img_url": "https://www.drivencoffee.com/wp-content/uploads/2016/03/Scandinavian-Blend-coffee.jpg" 
+    }, {
+        "id": 1,
+        "name":"Scandavian Blend",
+        "region": "Indonesia",
+        "roast_level": "Dark",
+        "price": "$75",
+        "description": "Earthly, dark choclate, medium body.  From Indonesia.",
+        "weights": "12oz, 3lb, 5lb",
+        "img_url": "https://www.drivencoffee.com/wp-content/uploads/2016/03/Scandinavian-Blend-coffee.jpg" 
+    }, {
+        "id": 2,
+        "name":"Scandavian Blend",
+        "region": "India",
+        "roast_level": "Light",
+        "price": "$25",
+        "description": "Earthly, dark choclate, medium body. From India.",
+        "weights": "12oz, 3lb, 5lb",
+        "img_url": "https://www.drivencoffee.com/wp-content/uploads/2016/03/Scandinavian-Blend-coffee.jpg" 
+    }, {
+        "id": 3,
+        "name":"Scandavian Blend",
+        "region": "Africa",
+        "roast_level": "Light",
+        "price": "$39",
+        "description": "Earthly, dark choclate, medium body.  From Africa",
+        "weights": "12oz, 3lb, 5lb",
+        "img_url": "https://www.drivencoffee.com/wp-content/uploads/2016/03/Scandinavian-Blend-coffee.jpg" 
+    }
+];
+
+var generateProductsList = function() {
+    products.forEach(function(item) {
+        var productElement = document.createElement("div");
+        // productElement.className = "card";
+        productElement.innerHTML = `
+        <div id="card-${item.id}" class="card" data-region="${item.region.toLowerCase()}">
+                    <div class="image-container">
+                            <img src="${item.img_url}" />
+                            <div class="overlay">
+
+                                    <div class="add_to_cart_btn">Add to Cart</div>
+                            </div>
+                    </div>
+
+                    <div class="container">
+                             <span class="product_price">${item.price}</span>
+                            <span class="product_name">${item.name}</span>
+                            <span class="roast_level">Roast Level: ${item.roast_level}</span>
+                            <span class="product_description"><i>${item.description}</i></span>
+                            <div class="product_rating">
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star"></span>
+                                    <span class="fa fa-star"></span>
+                            </div>
+                            <span class="product_weights">Weights: ${item.weights}</span>
+
+                    </div>
+    </div> `;
+
+    var productsGrid = document.querySelector(".products-grid");
+    
+    productsGrid.appendChild(productElement);
+
+    });
+}
+
+$(function() {
+    generateProductsList();
 });
+
+// var init = function() {
+//     alert("hi");
+//     generateProductsList();
+// }
+
+// return {
+//     init: init
+// };
+
+    
+}); // end of $(document).ready
+
