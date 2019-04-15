c7d88603815b2d1f80ed63c02b699c009ff8f3da
ngc2392 <phillicon4733@gmail.com>
2019-04-15 09:49:30 -0400

    added planet images

---
+++ ResponsiveDesignAssignment/js/script.js
@@ -0,0 +1,47 @@
+// filter closest to the sun, farthest from the sun 
+
+var data = [
+
+    {
+        "id":"",
+        "planetName":"Earth",
+        "imageURL":"images/planets/earth.jpg"
+    },{
+        "id": "",
+        "planetName": "Jupiter",
+        "imageURL": "images/planets/jupiter.jpg"
+    }, {
+        "id": "",
+        "planetName": "Mars",
+        "imageURL": "images/planets/mars.jpg", 
+        "links":["https://mars.nasa.gov/", "https://mars.nasa.gov/msl/multimedia/images/"]
+    }, {
+        "id": "",
+        "planetName": "Mercury",
+        "imageURL": "images/planets/mercury.jpg"
+    }, {
+        "id": "",
+        "planetName": "Neptune",
+        "imageURL": "images/planets/neptune.jpg"
+    }, {
+        "id": "",
+        "planetName": "Saturn",
+        "imageURL": "images/planets/saturn.jpg"
+    }, {
+        "id": "",
+        "planetName": "Uranus",
+        "imageURL": "images/planets/uranus.jpg"
+    }, {
+        "id": "",
+        "planetName": "Venus",
+        "orderFromTheSun":3,
+        "moons":"",
+        "imageURL":"images/planets/venus.jpg",
+        "links":["https://www.nasa.gov/venus", "https://solarsystem.nasa.gov/planets/venus/overview/"]
+    }
+];
+
+$(document).ready(function() {
+    
+
+});
